6d060bba4bbb37dfe81756e21f46c2305f44295c
nelpno <nelpno@gmail.com>
2022-06-28 20:29:44 -0300

    mudancas em teste

---
+++ desafio_dia_3/teste.js
@@ -0,0 +1,11 @@
+const linguagem = prompt("argumento1gumento2”]);
+
+alert("Olá " + nome +", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
+
+const gostaDeEstudar = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
+
+if (gostaDeEstudar == 1) {
+	alert("Muito bom! Continue estudando e você terá muito sucesso.");
+} else {
+	alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
--- java JRE e JDK/Programa.java
+++ java JRE e JDK/Programa.java
@@ -1,6 +1,5 @@
 class Programa {
-
     public static void main(String[] args) {
-        System.out.println("ola mundo");
+        System.out.println(" Olá mundo! Tudo bem com vocês?");
     }
 }
