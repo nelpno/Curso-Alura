dd295bb18adb9b8b9c8679f716e03a708ef474d0
nelpno <nelpno@gmail.com>
2022-06-28 22:50:14 -0300

    desafio dia 3 concluido

--- desafio_dia_3/teste.js
+++ desafio_dia_3/teste.js
@@ -1,11 +1,21 @@
-const linguagem = prompt("argumento1gumento2”]);
 
-alert("Olá " + nome +", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
+function perguntas () {
 
-const gostaDeEstudar = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
+var pergunta1 = prompt("1 - Seguir para área de Front-End \n2 - Seguir para a área de Back- End", ["1 ou 2"]);
 
-if (gostaDeEstudar == 1) {
-	alert("Muito bom! Continue estudando e você terá muito sucesso.");
+if (pergunta1 == 1) {
+	var pergunta2FrontEnd = prompt("1 - Seguir para área de React \n2 - Seguir para a área de Vue", ["1 ou 2"]);
 } else {
-	alert("Ahh que pena... Já tentou aprender outras linguagens?");
+var pergunta3BackEnd = prompt("1 - Seguir para área de C# \n2 - Seguir para a área de Java", ["1 ou 2"]);
 }
+var pergunta4Area = prompt("1 - Seguir se especializando na área escolhida \n2 - Seguir se desenvolvendo para se tornar Fullstack", ["1 ou 2"]);
+
+var pergunta5Area = prompt("Quais são as tecnologias nas quais você gostaria de se especializar?", ["Responda uma por vez"]);
+
+while (pergunta5Area) {
+	pergunta5Area = prompt("Tem mais alguma tecnologia que você gostaria de aprender?", ["Responda uma por vez"]);
+}
+
+}
+
+perguntas();
