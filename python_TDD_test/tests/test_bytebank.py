fddfbb85356f0efe4eb5224432526ff2714d24ae
nelpno <nelpno@gmail.com>
2022-11-03 19:15:58 -0300

    varios commits

--- python_TDD_test/tests/test_bytebank.py
+++ python_TDD_test/tests/test_bytebank.py
@@ -1,3 +1,7 @@
+import pytest
+from pytest import mark
+
+
 from codigo.bytebank import Funcionario
 
 
@@ -12,31 +16,43 @@ class TesteClass:
         assert resultado == esperado  # Then-desfecho
 
     def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
-        entrada = ' Lucas Carvalho ' #Given
+        entrada = ' Lucas Carvalho '  # Given
         esperado = 'Carvalho'
 
         lucas = Funcionario(entrada, '11/11/2000', 1111)
-        resultado = lucas.sobrenome() # When
+        resultado = lucas.sobrenome()  # When
 
-        assert resultado == esperado # Then
+        assert resultado == esperado  # Then
 
+    @mark.skip
     def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
-        entrada_salario = 100000 # Given
+        entrada_salario = 100000  # Given
         entrada_nome = 'Paulo Bragança'
         esperado = 90000
 
         funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
-        funcionario_teste.decrescimo_salario() # When
+        funcionario_teste.decrescimo_salario()  # When
         resultado = funcionario_teste.salario
 
-        assert resultado == esperado # Then
+        assert resultado == esperado  # Then
 
-
+    @mark.calcular_bonus
     def test_quando_calcula_bonus_recebe_1000_deve_retornar_100(self):
-        entrada = 1000 # Given
+        entrada = 1000  # Given
         esperado = 100
 
         funcionario_teste = Funcionario('Jose Paulo', '11/11/2000', entrada)
-        resultado = funcionario_teste.calcular_bonus() # When
+        resultado = funcionario_teste.calcular_bonus()  # When
 
-        assert resultado == esperado # Then
+        assert resultado == esperado  # Then
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_1000000_deve_retornar_exception(self):
+        with pytest.raises(Exception):
+            entrada = 1000000  # Given
+
+            funcionario_teste = Funcionario('Jose Paulo', '11/11/2000', entrada)
+            resultado = funcionario_teste.calcular_bonus()  # When
+
+            assert resultado  # Then
+
