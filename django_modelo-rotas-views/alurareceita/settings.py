758ac8cf28bfdb0f855e15c81335b48c06e916fc
nelpno <nelpno@gmail.com>
2022-11-17 08:29:13 -0300

    criando admin

--- django_modelo-rotas-views/alurareceita/settings.py
+++ django_modelo-rotas-views/alurareceita/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': '171261',
+        'HOST': 'localhost'
     }
 }
 
