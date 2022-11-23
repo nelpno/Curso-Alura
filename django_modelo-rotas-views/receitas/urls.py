2a916690e2ddc577cffe7096507381ddea045fc7
nelpno <nelpno@gmail.com>
2022-11-23 07:26:28 -0300

    projeto finalizado

--- django_modelo-rotas-views/receitas/urls.py
+++ django_modelo-rotas-views/receitas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from receitas.views import index, receita
+from receitas.views import index, receita, buscar
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:receita_id>', receita, name='receita'),
+    path('buscar', buscar, name='buscar')
 ]
