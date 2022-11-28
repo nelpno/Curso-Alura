a9f99925bb8baae315ffad807d1616252e58e077
nelpno <nelpno@gmail.com>
2022-11-28 08:02:42 -0300

    finalizado os recursos de api

--- django_rest_api/escola/serializer.py
+++ django_rest_api/escola/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +7,34 @@ class AlunoSerializer(serializers.ModelSerializer):
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
 
 class CursoSerializer(serializers.ModelSerializer):
+    nivel = serializers.SerializerMethodField()
     class Meta:
         model = Curso
         fields = '__all__'
+    def get_nivel(self, obj):
+        return obj.get_nivel_display()
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    aluno = serializers.ReadOnlyField(source='aluno.nome')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = '__all__'
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source = 'aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
